543120ecf55a831c269380235a03bad94eea53ca
eug-vs <eugene@eug-vs.xyz>
2025-11-14 14:31:55 +0100

    feat: disable trailing commas for jsonc
    We want to use turbo.jsonc format but it raises annoying warnings in
    IDE's. Relavant discussion:
    
    https://github.com/prettier/prettier/issues/15956#issuecomment-2853235624

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ module.exports = {
   trailingComma: "es5",
   tabWidth: 2,
   semi: true,
-  singleQuote: false
+  singleQuote: false,
+  overrides: [
+    {
+      files: ["**/*.jsonc"],
+      options: {
+        trailingComma: "none",
+      },
+    },
+  ],
 };
